9b99b87359b76310a5286c7f121b3a9c55b152f8
XiangyuShen <xshen2006@yahoo.com>
2021-10-26 16:47:37 -0400

    Styling and persistence across sessions

--- src/pages/DisplayNotes.js
+++ src/pages/DisplayNotes.js
@@ -1,33 +1,56 @@
+import React, { Component } from "react";
 import { List, Fab, withStyles } from "@material-ui/core";
 import { Add } from "@material-ui/icons";
 import Note from "../components/Note";
+import Search from "../components/Search";
 import { Link } from "react-router-dom";
-
-
 const styles = {
-  fab: {
-    position: 'absolute',
-    bottom: "2rem",
-    right: "2rem",
-  }
-};
-
-function DisplayNotes(props) {
-    const { notes, deleteNote, classes } = props;
-  return (
-    <>
-        <List>
-            {notes.map((note, index) => {
-                return <Note note={note} key={index} deleteNote={deleteNote} />;
+    fab: {
+      position: 'absolute',
+      bottom: "2rem",
+      right: "2rem",
+    }
+  };
+class DisplayNotes extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        query: "",
+      };
+    }
+  
+    updateQuery = (query) => {
+      this.setState({ query });
+    };
+  
+    includes = (note) => {
+      const query = this.state.query.trim().toLowerCase();
+      return (
+        query === "" ||
+        note.title.toLowerCase().includes(query) ||
+        note.text.toLowerCase().includes(query)
+      );
+    };
+  
+    render() {
+      const { notes, deleteNote, classes } = this.props;
+      return (
+        <>
+          <Search query={this.state.query} updateQuery={this.updateQuery} />
+          <List>
+            {notes.filter(this.includes).map((note, index) => {
+              return <Note note={note} key={index} deleteNote={deleteNote} />;
             })}
-        </List>
-        <Link to="/add">
+          </List>
+          <Link to="/add">
             <Fab aria-label={"Add"} className={classes.fab}>
-                <Add />
+              <Add />
             </Fab>
-        </Link>
-    </>
-  );
-}
-
-export default withStyles(styles)(DisplayNotes);
+          </Link>
+        </>
+      );
+    }
+  }
+  
+  export default withStyles(styles)(DisplayNotes);
+  
